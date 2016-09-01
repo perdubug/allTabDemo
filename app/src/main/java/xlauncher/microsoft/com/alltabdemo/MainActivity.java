f98e55b05a90e519c1acd6f5ee9256c23f5c428f
Yang Ming <yangming@microsoft.com>
2016-09-01 14:34:33 +0800

    Version zero

---
+++ app/src/main/java/xlauncher/microsoft/com/alltabdemo/MainActivity.java
@@ -0,0 +1,13 @@
+package xlauncher.microsoft.com.alltabdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
